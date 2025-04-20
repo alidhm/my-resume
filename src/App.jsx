fbebf63d6bcb6b04aac641ab8f217398434c3ac7
alidhm <ali7094.dd@gmail.com>
2025-04-20 19:18:19 +0330

    remove previous header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
 import { useState } from 'react';
-import { Header } from './components/Header';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Home from './pages/home';
